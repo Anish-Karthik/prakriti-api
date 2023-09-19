b08f2427e11998a3063a8c8608a96b38b1cb8986
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-09-19 14:37:16 +0530

    AI

--- app.py
+++ app.py
@@ -1,42 +1,21 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$150,000'
-  }
-]
 
-@app.route("/")
-def hello_jovian():
-    return render_template('home.html', 
-                           jobs=JOBS, 
-                           company_name='Jovian')
+regmodel=pickle.load(open('prakriti-classifier.pkl','rb'))
 
-@app.route("/api/jobs")
-def list_jobs():
-  return jsonify(JOBS)
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    data = np.array([list(data)])
+    print(data)
+    output=regmodel.predict(data)
+    print(output[0])
+    return jsonify(output[0])
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
