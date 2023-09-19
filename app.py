aae8f2acc2997ffe4b13cbe40ffee7aae4f017b4
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-09-19 15:24:08 +0530

    AI

--- app.py
+++ app.py
@@ -1,47 +1,12 @@
 from flask import Flask,request, render_template, jsonify
 import numpy as np
-import pickle
+import joblib
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$150,000'
-  }
-]
-
-@app.route("/")
-def hello_jovian():
-    return render_template('home.html', 
-                           jobs=JOBS, 
-                           company_name='Jovian')
-
-@app.route("/api/jobs")
-def list_jobs():
-  return jsonify(JOBS)
 
 ## Load the model
-regmodel=pickle.load(open('prakriti-classifier.pkl','rb'))
+regmodel=joblib.load('prakriti-classifier.joblib')
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
